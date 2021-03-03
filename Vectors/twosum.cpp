0c2e9c3e4ece59936e9551435a1998920711e8d2
everly <everlyprecia@gmail.com>
2021-03-03 20:20:28 +0530

    Add Two Sum

---
+++ Vectors/twosum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+	public:
+		    vector<int> twoSum(vector<int>& nums, int target) {
+			            int x;
+				            for(int i=0;i<nums.size()-1;i++){
+						            x=target-nums[i];
+							                for(int j=i+1;j<nums.size();j++){
+										              if(nums[j]==x){
+												                        return{i,j};
+															              }  
+											                  }
+									        }
+					            return {};
+						        }
+};
